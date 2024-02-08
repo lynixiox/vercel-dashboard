6b92838ff695868fbc02a80f5ea6d0065d45245d
Gandalf <lynxdev@protonmail.com>
2024-02-08 15:04:08 +0200

    delete and update invoicing system

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import {z} from 'zod'
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -10,7 +12,27 @@ const FormSchema = z.object({
     date: z.string()
 })
 
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
 const CreateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData){
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    })
+
+    const amountInCents = amount * 100;
+    
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amount}, status = ${status} WHERE id = ${id}
+    `
+
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices")
+}
 
 export async function createInvoice(formData:FormData) {
     const {customerId, amount, status} = CreateInvoice.parse({
@@ -26,4 +48,16 @@ export async function createInvoice(formData:FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+
 }   
+
+
+export async function deleteInvoice(id: string){
+    await sql`
+        DELETE FROM invoice WHERE id = ${id}
+    `;
+    revalidatePath("/dashboard/invoices")
+}
