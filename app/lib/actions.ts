df6a8cff7fec3e4d41822090359c76475287580f
Gandalf <lynxdev@protonmail.com>
2024-02-08 22:04:43 +0200

    added login but broker the css

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,29 @@ import {z} from 'zod'
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData
+){
+    try{
+        await signIn('credentials', formData);
+    }catch(error){
+        if(error instanceof AuthError){
+            switch(error.type){
+                case 'CredentialsSignin':
+                    return 'Invalid Credentials';
+                default:
+                    return "Something went wrong";
+            }
+        }
+        throw error;
+    }
+}
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
@@ -70,6 +93,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
     //prepare data for insertion into the database
     const {customerId, amount, status} = validatedFields.data;
+
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
    
---
+++ app/login/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import LoginForm from '../ui/login-form'
+const LoginPage = () => {
+  return (
+   <main className='flex items-center justify-center md:h-screen'>
+    <div className='relativ mx-auto flex w-full max-w-[400] flex-col space-y-2.5 p-4 md:-mt-32'>
+        <div className='flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36'>
+            <div className='w-32 text-white md:w-36'>
+                TicketCrew
+            </div>
+        </div>
+        <LoginForm/>
+    </div>
+   </main>
+  )
+}
+
+export default LoginPage
